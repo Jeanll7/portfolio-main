f8bd8f6cf561aeda70035f162f525b6091286e49
devjean <jeancarloleall@gmail.com>
2022-11-09 01:29:55 -0300

    flex-start

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ acordeonTriggers.forEach((trigger) => {
     trigger.addEventListener('click', (e) => {
         const acordeon = trigger.parentElement
         const isOpen = acordeon.classList.contains('open')
-        
+
         if (isOpen) {
             acordeon.classList.remove('open')
         } else {
