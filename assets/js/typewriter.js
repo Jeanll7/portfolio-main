749b257bb8fbdc171c08a098e02545fa4c70332d
devjean <jeancarloleall@gmail.com>
2022-11-14 14:42:14 -0300

    frames

---
+++ assets/js/typewriter.js
@@ -0,0 +1,10 @@
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    for (let i = 0; i < textArray.length; i++) {
+        setTimeout(() => el.innerHTML += textArray[i], 90 * i);
+    }
+}
+
+const title = document.querySelector('.typewriter');
+typeWriter(title);
