dd4c66a400fa46c736ea5cf87b305f1d090661db
devjean <jeancarloleall@gmail.com>
2022-11-15 09:36:11 -0300

    color icon

--- assets/js/typewriter.js
+++ assets/js/typewriter.js
@@ -5,6 +5,6 @@ function typeWriter(el) {
     const textArray = el.innerHTML.split('');
     el.innerHTML = '';
     for (let i = 0; i < textArray.length; i++) {
-        setTimeout(() => el.innerHTML += textArray[i], 90 * i);
+        setTimeout(() => el.innerHTML += textArray[i], 110 * i);
     }
 }
